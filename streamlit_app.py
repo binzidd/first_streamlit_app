dbadc51459f04c600c696ad91d803fbe6b3e62b4
Binay <binzidd@gmail.com>
2022-12-07 23:52:52 +1100

    Update streamlit_app.py
    added a new header section
    added a json parser as text

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ fruit_to_show=my_fruit_list.loc[fruit_selected]
 
 streamlit.dataframe(fruit_to_show) 
 
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
