6e5a214687989f2d65ccd28c4c832216473d8f7b
Binay <binzidd@gmail.com>
2022-12-08 02:27:03 +1100

    Update streamlit_app.py
    indentation issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ try:
   else:
     back_from_funciton=get_fruitvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-    except URLError as e:
+  except URLError as e:
   streamlit.error()
 
 #do not run anything past here while we check
