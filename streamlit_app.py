32089ec2cf5aaa18be1b0cf2fe965e372c6ac1d7
Binay <binzidd@gmail.com>
2022-12-07 23:51:29 +1100

    Update streamlit_app.py
    added response library and calling it via a new variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -17,3 +18,7 @@ fruit_selected=streamlit.multiselect("Pick some fruits:", list (my_fruit_list.in
 fruit_to_show=my_fruit_list.loc[fruit_selected]
 
 streamlit.dataframe(fruit_to_show) 
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
