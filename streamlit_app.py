b98c3b9823d7cd35ff9189f3a4b77e1027f9c644
Binay <binzidd@gmail.com>
2022-12-08 00:01:29 +1100

    Update streamlit_app.py
    Interface calls from user using streamlit.text_input and parameterised value of fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,11 @@ fruit_to_show=my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruit_to_show) 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#adding interface code 
+fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi') #input data
+streamlit.write('The user entered',fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice) #parametrised call for fruit choice
 #streamlit.text(fruityvice_response.json()) no longer needed
 
 ##normalise json
