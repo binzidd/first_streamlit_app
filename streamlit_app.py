caed6540f35d0bf6786a700be75230135e93fac9
Binay <binzidd@gmail.com>
2022-12-07 14:54:20 +1100

    Update streamlit_app.py
    Added interaction based table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list) 
+fruit_selected=streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','Strawberries'])
+fruit_to_show=my_fruit_list.loc[fruit_selected]
+
+streamlit.dataframe(fruit_to_show) 
