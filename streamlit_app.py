41d8495ae3fdcfe9a8b7870e478511f1c9148e65
Binay <binzidd@gmail.com>
2022-12-07 14:58:23 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title("My Guzla New Healthy Diner")
+streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
